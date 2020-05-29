448ba293427623ad529fdbf0dc8347356a0eef48
Bruno Paula <brunopaula@dryserv.com>
2020-05-28 20:04:07 -0400

    added hover effect

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
- 
-function onClickMenu(){
-	// document.getElementById("menu").classList.toggle("change");
-	 document.getElementById("nav").classList.toggle("change");
-	 document.getElementById("menu-bg").classList.toggle("change-bg");
-
+function onClickMenu() {
+  // document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
 }
